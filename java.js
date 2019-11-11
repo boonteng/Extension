3e58822060b96de59a46c9c8fcb755738f51ad3b
BerniceTeoh <41535667+BerniceTeoh@users.noreply.github.com>
2019-11-11 20:27:49 +0800

    Update java.js

--- java.js
+++ java.js
@@ -1,5 +1,5 @@
 
-const MyNotes = document.getElementById('mnotes');
+const MyNote = document.getElementById('mnotes');
 const gettingItem = browser.storage.local.get('MyAllNotes');
 gettingItem.then((res) => {
 try{
